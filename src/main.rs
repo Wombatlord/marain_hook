41fe85989d37df0492f21e3ed0c0685c8c7121e3
Wombatlord <jordan.stone0@gmail.com>
2024-02-04 00:57:02 +0000

    logs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use warp::{
     http::{Response, StatusCode},
     Filter,
 };
+use chrono::Utc;
 
 enum HookResult {
     Ok,
@@ -24,6 +25,27 @@ enum Script {
 
 const MARAIN_SOURCE_DIR: &str = "/var/www/marain";
 
+enum LogLevel {
+    INFO,
+    ERROR,
+}
+
+impl std::fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use LogLevel::*;
+        let s: String = match &self {
+            INFO => "INFO".into(),
+            ERROR => "ERROR".into()
+        };
+        write!(f, "{s}")
+    }
+}
+
+fn log(level: LogLevel, msg: impl std::fmt::Display) {
+    let time = Utc::now();
+    println!("{time} - {level} - {msg}");
+}
+
 fn get_script_argv(script: Script) -> &'static [&'static str] {
     match (script, getenv("HOOK_ENV")) {
         (_, x) if x == String::from("dev") => &["echo", "foo"],
@@ -33,23 +55,35 @@ fn get_script_argv(script: Script) -> &'static [&'static str] {
 }
 
 fn update_and_relaunch() -> HookResult {
+    log(LogLevel::INFO, "Update hook called");
     let Ok(mut update_proc) = Popen::create(get_script_argv(Script::Update), PopenConfig::default()) else {
+        log(LogLevel::ERROR, "Failed to create update subprocess");
         return HookResult::ServerError;
     };
 
+    log(LogLevel::INFO, "Waiting for update process");
     match update_proc.wait() {
         Ok(subprocess::ExitStatus::Exited(0)) => {
+            log(LogLevel::INFO, "Update process successful.");
             let update_result = HookResult::Ok;
 
             let Ok(mut relaunch_proc) = Popen::create(get_script_argv(Script::Relaunch), PopenConfig::default())
             else {
+                log(LogLevel::ERROR, "Failed to create relaunch subprocess");
                 return HookResult::ServerError;
             };
+            log(LogLevel::INFO, "Relaunch subprocess created, detaching.");
             relaunch_proc.detach();
             update_result
         },
-        Ok(_fail) => HookResult::UpdateFailed,
-        Err(_) => HookResult::ServerError,
+        Ok(fail) => {
+            log(LogLevel::ERROR, format!("Update failed with exit code: {fail:?}"));
+            HookResult::UpdateFailed
+        },
+        Err(e) => {
+            log(LogLevel::ERROR, format!("Update subprocess wait failed with error: {e}"));
+            HookResult::ServerError
+        },
     }
 }
 
@@ -72,6 +106,6 @@ async fn main() {
                     .body("".to_string()),
             }
         });
-
+    log(LogLevel::INFO, "Hook server starting");
     warp::serve(update).run(([0, 0, 0, 0], 42069)).await;
 }
